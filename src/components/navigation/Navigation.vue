a23d00e2728eb0bbafd8edc0eb8c8bb05c7f9003
marionstaats <marionstaats1@gmail.com>
2025-05-17 21:00:51 +0200

    Fix nav slider and update vite package

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -12,7 +12,7 @@ const route = useRoute()
   <div class="navigation d-flex justify-space-between">
     <Logo />
     <div class="d-flex align-center px-4">
-      <v-tabs slider-color="primary" :hide-slider="route.name === Routes.Home" align-tabs="end">
+      <v-tabs slider-color="primary" :hide-slider="route.name === Routes.Home || route.name === undefined" align-tabs="end">
         <v-tab v-for="({ title, to }, i) in mainNav" :key="i" :to="{ name: to }">{{ title }}</v-tab>
       </v-tabs>
       <ThemeSwitch />
